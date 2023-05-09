4dc1471c4fd037205cef8439473e0c1db78c14ee
khuephamy <khuephumy19@gmail.com>
2023-05-09 15:23:35 -0500

    feature openai route

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import { Configuration, OpenAIApi } from "openai";
+import openAiRoutes from "./routes/openai.js";
 
 /* CONFIG */
 dotenv.config();
@@ -20,7 +21,10 @@ app.use(cors());
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
-const openai = new OpenAIApi(configuration);
+export const openai = new OpenAIApi(configuration);
+
+/* ROUTES */
+app.use("/openai", openAiRoutes);
 
 const PORT = process.env.PORT || 9000;
 
