1692a7d0e0c6224dbaeef1d0ee52ba9afc2d87c4
khuephamy <khuephumy19@gmail.com>
2023-05-12 12:49:07 -0500

    made chatbot work

--- routes/openai.js
+++ routes/openai.js
@@ -1,7 +1,7 @@
 import express from "express";
 import axios from "axios";
 import dotenv from "dotenv";
-import { openai } from "../index";
+import { openai } from "../index.js";
 
 dotenv.config();
 const router = express.Router();
@@ -9,6 +9,29 @@ const router = express.Router();
 router.post("/text", async (req, res) => {
   try {
     const { text, activeChatId } = req.body;
+    console.log(req.body);
+
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: "system", content: "You are a helpful assistant." }, // this represents the bot and what role they will assume
+        { role: "user", content: text }, // the message that the user sends
+      ],
+    });
+
+    await axios.post(
+      `https://api.chatengine.io/chats/${activeChatId}/messages/`,
+      { text: response.data.choices[0].message.content },
+      {
+        headers: {
+          "Project-ID": process.env.PROJECT_ID,
+          "User-Name": process.env.BOT_USER_NAME,
+          "User-Secret": process.env.BOT_USER_SECRET,
+        },
+      }
+    );
+
+    res.status(200).json({ text: response.data.choices[0].message.content });
   } catch (error) {
     console.error("error", error);
     res.status(500).json({ error: error.message });
