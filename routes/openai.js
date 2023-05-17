c261d9197e17f9709e11bc402c33c68a78b5e658
khuephamy <khuephumy19@gmail.com>
2023-05-16 20:52:53 -0500

    ai code

--- routes/openai.js
+++ routes/openai.js
@@ -38,4 +38,36 @@ router.post("/text", async (req, res) => {
   }
 });
 
+router.post("/code", async (req, res) => {
+  try {
+    const { text, activeChatId } = req.body;
+    console.log(req.body);
+
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: "system", content: "You are an assistant coder who responds with only code and no explanations." }, // this represents the bot and what role they will assume
+        { role: "user", content: text }, // the message that the user sends
+      ],
+    });
+
+    await axios.post(
+      `https://api.chatengine.io/chats/${activeChatId}/messages/`,
+      { text: response.data.choices[0].message.content },
+      {
+        headers: {
+          "Project-ID": process.env.PROJECT_ID,
+          "User-Name": process.env.BOT_USER_NAME,
+          "User-Secret": process.env.BOT_USER_SECRET,
+        },
+      }
+    );
+
+    res.status(200).json({ text: response.data.choices[0].message.content });
+  } catch (error) {
+    console.error("error", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
 export default router;
