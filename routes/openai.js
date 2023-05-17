1c8d225f4dc24f2b18b2d571a783a4613210f9a2
khuephamy <khuephumy19@gmail.com>
2023-05-17 14:14:19 -0500

    Ai assist text 2

--- routes/openai.js
+++ routes/openai.js
@@ -71,4 +71,27 @@ router.post("/code", async (req, res) => {
   }
 });
 
+router.post("/assist", async (req, res) => {
+  try {
+    const { text, activeChatId } = req.body;
+
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are a helpful assistant that serves to only complete user's thoughts or sentences.",
+        }, // this represents the bot and what role they will assume
+        { role: "user", content: `Finish my thought: ${text}` }, // the message that the user sends
+      ],
+    });
+
+    res.status(200).json({ text: response.data.choices[0].message.content });
+  } catch (error) {
+    console.error("error", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
 export default router;
